a393c0eaa42296349c55f6adcc3824a0303b1636
intisarhaqueLU <intisar@legalutopia.co.uk>
2021-11-16 13:57:21 +0000

    removing header C

--- geeks/challenges/school/headers/student.h
+++ geeks/challenges/school/headers/student.h
@@ -1,24 +1,15 @@
-// void add(int a, int b)
-// {
-//     printf("Added value=%d\n", a + b);
-// }
-// void multiply(int a, int b)
-// {
-//     printf("Multiplied value=%d\n", a * b);
-// }
-
-#ifndef STUDENT_H_INCLUDED
-#define STUDENT_H_INCLUDED 
-
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
-void add(int, int);
-void multiply(int, int);
-void subtract(int, int);
-
-
-
-#endif 
+void add(int a, int b)
+{
+    printf("Added value=%d\n", a + b);
+}
+void multiply(int a, int b)
+{
+    printf("Multiplied value is %d \n", a * b);
+}
+void subtract(int a, int b)
+{
+    printf("subtracted value is %d \n", a-b);
+}
--- geeks/challenges/school/headers/subject.h
+++ geeks/challenges/school/headers/subject.h
@@ -1,12 +1,30 @@
-#ifndef STUDENT_H_INCLUDED
-#define STUDENT_H_INCLUDED 
-#endif 
-
+#ifndef SUBJECT_H
+#define SUBJECT_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void test();
-char* getScoreType(enum ScoreType);
+enum scoreType {Exam, Coursework, Practical};
 
 
+struct subject
+{
+    char subjectTitle[15];
+    char examType;
+};
+
+void test(){
+    enum scoreType a = Exam;
+    printf("scoretype is %d\n", a);
+
+}
+
+char* getScoreType(enum ScoreType typea){
+    switch(typea){
+        case Exam: return "Exam";
+        case Coursework: return "Coursework";
+        case Practical: return "Practical";
+    }
+}
+
+#endif
