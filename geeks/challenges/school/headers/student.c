1c296fb521125483a54b8d166bf746097e074d5d
intisarhaqueLU <intisar@legalutopia.co.uk>
2021-11-16 10:27:17 +0000

    school challenge

---
+++ geeks/challenges/school/headers/student.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void add(int a, int b)
+{
+    printf("Added value=%d\n", a + b);
+}
+void multiply(int a, int b)
+{
+    printf("Multiplied value is %d\n", a * b);
+}
