07f03ba1d461a7d29ac854a0ac9f1c5d25b17f3a
Intisar Haque <arsenal.15@hotmail.co.uk>
2021-12-06 09:47:02 +0000

    thread challange done

--- geeks/threads/udemy_challenge.c
+++ geeks/threads/udemy_challenge.c
@@ -8,19 +8,32 @@
 
 /*GLOBALS*/
 int counter = 0;
-//INITIALIZE MUTEX IDIOT
 pthread_mutex_t lock1 = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t condition_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t condition_cond = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t condition_cond = PTHREAD_COND_INITIALIZER;
 /*GLOBALS*/
-//even message first then odd
+
 
 void * thread_function(void *ptr){
     int message = *((int*)ptr);
+
     pthread_mutex_lock(&lock1);
-    counter++;
+    if ((message%2)==0){      
+        counter++;
+    }
+    else if ((message%2)!=0){
+        //need if statement because some odd threads get to this bit after the broadcast
+        if (counter<(NTHREADS/2)){
+            printf("Thread %d waiting\n", message);
+            pthread_cond_wait(&condition_cond, &lock1);
+        }
+        counter++;
+    }
     printf("Thread number %ld\t Message is %d\t Modified counter to %d\n", pthread_self(), message, counter);
     printf("Thread number %ld\t Message is %d\t Read counter as %d\n", pthread_self(), message, counter);
+    if(counter==5){
+        printf("Now broadcasting\n");
+        pthread_cond_broadcast(&condition_cond);
+    }
     pthread_mutex_unlock(&lock1);
 
     return (NULL);
@@ -29,7 +42,6 @@ void * thread_function(void *ptr){
 
 int main(){
     pthread_t thread_id[NTHREADS];
-
     int i, j;
 
     //create threads
@@ -42,15 +54,15 @@ int main(){
         *arg = i;
         pthread_create(&thread_id[i], NULL, &thread_function, (void*)arg);
     }
-    //sleep(3);
+
+    sleep(1);
+
     //join theads
     for (int j=0; j<NTHREADS; j++){
         pthread_join(thread_id[j], NULL);
-        //pthread_exit(&thread_id[j]);
     }
 
     printf("Final counter value: %d\n", counter);
-
     pthread_exit(NULL);
     
     return 0;
